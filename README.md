8596f22034957cd77d583a2cd496951270343ab8
Raja Rao DV <rajar@vmware.com>
2012-12-24 12:41:13 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+<h1>An Express + Socket.io based chat app that uses Redis as session store & RabbitMQ for PubSub</h1>
+
+<h3>Getting started:</h3>
+<ul>
+<li><a href="http://redis.io">RabbitMQ</a> must be installed and running.
+
+<li><a href="http://redis.io">Redis</a> must be installed and running.
+<li><a href="http://nodejs.org">Node.js</a> must be installed
+<li>`cd rabbitpubsub` & run `npm install` </li>
+<li>`$ node app.js` </li>
+
+
+Fire up your favorite browser and go to "localhost:3000". Open more browser windows to see it in work.
+
+
+<h1>Credits</h1>
+<p>
+PS: Original: <a href="https://github.com/steffenwt/nodejs-pub-sub-chat-demo">https://github.com/steffenwt/nodejs-pub-sub-chat-demo</a></p>
+<p>
+Updated: HTML UI part is mostly original but Node.js server part is completely rewritten to use RabbitMQ, Redis, Express 3.x, Socket.io, Socket.Sessions, & Cloud Foundry</p>
+</p>
---
+++ app.js
@@ -0,0 +1,127 @@
+var express = require('express')
+    , routes = require('./routes')
+    , http = require('http')
+    , path = require('path')
+    , redis = require('redis')
+    , amqp = require('amqp');
+
+
+var rabbitConn = amqp.createConnection({});
+var chatExchange;
+rabbitConn.on('ready', function () {
+    chatExchange = rabbitConn.exchange('chatExchange', {'type':'fanout'});
+});
+
+
+/*
+ Setup Express & Socket.io
+ */
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+//Set xhr-polling as WebSocket is not supported by CF
+io.set("transports", ["xhr-polling"]);
+
+//Set Socket.io's log level to 1 (info). Default is 3 (debugging)
+io.set('log level', 1);
+
+
+/*
+ Also use Redis for Session Store. Redis will keep all Express sessions in it.
+ */
+var RedisStore = require('connect-redis')(express),
+    rClient = redis.createClient(),
+    sessionStore = new RedisStore({client:rClient});
+
+var cookieParser = express.cookieParser('your secret here');
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+
+    /*
+     Use cookieParser and session middlewares together.
+     By default Express/Connect app creates a cookie by name 'connect.sid'.But to scale Socket.io app,
+     make sure to use cookie name 'jsessionid' (instead of connect.sid) use Cloud Foundry's 'Sticky Session' feature.
+     W/o this, Socket.io won't work if you have more than 1 instance.
+     If you are NOT running on Cloud Foundry, having cookie name 'jsessionid' doesn't hurt - it's just a cookie name.
+     */
+    app.use(cookieParser);
+    app.use(express.session({store:sessionStore, key:'jsessionid', secret:'your secret here'}));
+
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+/*
+ When the user logs in (in our case, does http POST w/ user name), store it
+ in Express session (which in turn is stored in Redis)
+ */
+app.post('/user', function (req, res) {
+    req.session.user = req.body.user;
+    res.json({"error":""});
+});
+
+/*
+ Use SessionSockets so that we can exchange (set/get) user data b/w sockets and http sessions
+ Pass 'jsessionid' (custom) cookie name that we are using to make use of Sticky sessions.
+ */
+var SessionSockets = require('session.socket.io');
+var sessionSockets = new SessionSockets(io, sessionStore, cookieParser, 'jsessionid');
+
+sessionSockets.on('connection', function (err, socket, session) {
+    /*
+     When a user sends a chat message, publish it to chatExchange w/o binding key (bindingKey doesn't matter
+     because chatExchange is fanout).
+     Notice that we are getting user's name from session.
+     */
+    socket.on('chat', function (data) {
+        var msg = JSON.parse(data);
+        var reply = {action:'message', user:session.user, msg:msg.msg };
+        // pub.publish('chat', reply);
+        chatExchange.publish('', reply);
+    });
+
+    /*
+     When a user joins, publish it to chatExchange w/o binding key (bindingKey doesn't matter
+     because chatExchange is fanout).
+     Notice that we are getting user's name from session.
+     */
+    socket.on('join', function () {
+        var reply = {action:'control', user:session.user, msg:' joined the channel' };
+        chatExchange.publish('', reply);
+    });
+
+
+    /*Initialize subscriber queue.
+     First create a queue w/o any name. This forces RabbitMQ to create new queue
+     for every socket.io connection w/ a new random queue name.
+     Then bind the queue to chatExchange and listen to ALL messages
+     Lastly, create a consumer (via .subscribe) that waits for messages from RabbitMQ. And when
+     a message comes, send it to the browser.
+
+     Notice that we are creating this w/in sessionSockets.on('connection' to create NEW queue for every connection
+     */
+    rabbitConn.queue('', {exclusive:true}, function (q) {
+        q.bind('chatExchange', "#"); // bind to chat ALL channel/bindingKey
+        q.subscribe(function (message) {
+            socket.emit('chat', JSON.stringify(message));
+        });
+    });
+});
+
+server.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
